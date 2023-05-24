5df7992e561bfa02ce5fd630fc464de934caee4c
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-05-24 11:13:13 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/FarmersMarket/FarmersMarketApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.FarmersMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmersMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmersMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/zhelezyaka/FarmersMarket/FarmersMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.FarmersMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmersMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
